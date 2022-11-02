e29a5b48481407f72be6f7bc0c1c05124fca5e70
kasang <2995991652@qq.com>
2022-11-02 23:03:08 +0800

    创建网关服务

---
+++ service_gateway/src/main/java/com/h2s/ggkt/GateWayApplication.java
@@ -0,0 +1,21 @@
+package com.h2s.ggkt;
+// -*-coding:utf-8 -*-
+
+/*
+ * File       : GateWayApplication.java
+ * Time       ：2022/11/2 22:47
+ * Author     ：hhs
+ * version    ：java8
+ * Description：
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(GateWayApplication.class, args);
+    }
+}
